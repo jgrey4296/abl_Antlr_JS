b91a57eccbec66203b0fd8c649fd7afcc0f3fd7a
John Grey <jgrey@soe.ucsc.edu>
2015-08-10 05:55:55 -0700

    added amdefine module, added simple throw error unit tests

--- js/ABLModule.js
+++ js/ABLModule.js
@@ -1,35 +1,41 @@
 if(define !== 'function'){
-  var define = require('amdefine')(module);
+    var define = require('amdefine')(module);
 }
 
 
 define(['./lib/antlr4/index','./ABLListener','./ABLLexer','./ABLParser','./JGListener'],function(antlr4,ABLListener,ABLLexer,ABLParser,JGListener){
-  //Setup:
+    //Setup:
+    
+    var parseInput = function(input,startRule){
+        if(startRule === undefined){
+            startRule = "prog";
+        }
+        if(input.length <= 0){
+            throw new Error("Parser given no input");
+        };
+        //console.log("Parsing Input:",input);
+        var chars = new antlr4.InputStream(input);
+        var lexer = new ABLLexer.ABLLexer(chars);
+        var tokens  = new antlr4.CommonTokenStream(lexer);
+        var parser = new ABLParser.ABLParser(tokens);
+        parser.buildParseTrees = true;
+
+        if(parser[startRule] === undefined){
+            throw new Error("Parser does not have a rule:",startRule);
+        }        
  
-  var parseInput = function(input){
-    if(input.length <= 0){
-      console.log("No input");
-      return [];
+
+        var tree = parser[startRule]();
+        var printer = new JGListener.JGListener();
+
+        antlr4.tree.ParseTreeWalker.DEFAULT.walk(printer,tree);
+
+
+        return printer.parsedStack;
+
     };
-    console.log("Parsing Input:",input);
-    var chars = new antlr4.InputStream(input);
-    var lexer = new ABLLexer.ABLLexer(chars);
-    var tokens  = new antlr4.CommonTokenStream(lexer);
-    var parser = new ABLParser.ABLParser(tokens);
-    parser.buildParseTrees = true;
-
-    //Start Rule:
-    var tree = parser.prog();
-    var printer = new JGListener.JGListener();
-
-    antlr4.tree.ParseTreeWalker.DEFAULT.walk(printer,tree);
 
 
-    return printer.parsedStack;
-
-  };
-
-
-  //Returning interface:
-  return { parse : parseInput};
+    //Returning interface:
+    return { parse : parseInput};
 });
--- js/test/unitTests.js
+++ js/test/unitTests.js
@@ -1,63 +1,72 @@
 var ABLModule = require('../ABLModule');
 
-//DEFAULT EMPTY STRING PARSE
-exports.EmptyStringTest = function(test){
-  test.expect(1);
-  var result = ABLModule.parse("");
-  test.ok(result.length === 0,"Empty String should produce empty Array");
+//Throw error when a non-existing start rule is specified:
+exports.throwErrorTests = {
 
-  test.done();
+    throw_Error_On_No_Start_Rule : function(test){
+        test.throws(function(){
+            ABLModule.parse("blah blah blah","nonexistentRule");
+        });
+        test.done();
+    },
+
+    throw_error_on_no_input_text : function(test){
+        test.throws(function(){
+            ABLModule.parse("");
+        });
+        test.done();
+    },
 };
 
-//----------
+//------------------------------
 //PACKAGE and IMPORT Tests
 exports.PackageOnly = function(test){
-  //test.expect(TODO);
-  var result = ABLModule.parse("package testPackage;");
-  test.ok(result.length === 1, "Result have one entry");
-  var package = result.shift();
-  test.ok(package[0] === 'package',"Object should be a package");
-  test.ok(package[1] === 'testPackage',"Object should be the testpackage");
-  test.done();
+    //test.expect(TODO);
+    var result = ABLModule.parse("package testPackage;");
+    test.ok(result.length === 1, "Result should have one entry");
+    var package = result.shift();
+    test.ok(package[0] === 'package',"Object should be a package");
+    test.ok(package[1] === 'testPackage',"Object should be the testpackage");
+    test.done();
 };
 
 exports.MultiplePackagesFail = function(test){
-  console.log("---------- STUB");
-  test.done();
+    console.log("---------- STUB");
+    test.done();
 };
 
 exports.BasicImport = function(test){
-  //test.expect(TODO);
-  var result = ABLModule.parse("package testPackage; import blah;");
-  test.ok(result.length === 2,"Should be 2 entries in the parsed result");
-  var package = result.shift();
-  var impStat = result.shift();
+    //test.expect(TODO);
+    var result = ABLModule.parse("package testPackage; import blah;");
+    test.ok(result.length === 2,"Should be 2 entries in the parsed result");
+    var package = result.shift();
+    var impStat = result.shift();
 
-  test.ok(impStat[0] === 'import',"Should be an import recognised");
-  test.ok(impStat[1] === 'blah',"Should be the import 'blah'");
-  test.done();
+    test.ok(impStat[0] === 'import',"Should be an import recognised");
+    test.ok(impStat[1] === 'blah',"Should be the import 'blah'");
+    test.done();
 };
 
 exports.importBeforePackage  = function(test){
-  console.log("---------- STUB");
-  test.done();
+    console.log("---------- STUB");
+    test.done();
 };
 
 
 
 exports.ImportTest = function(test){
-  console.log("---------- STUB");
-  test.done();
+    console.log("---------- STUB");
+    test.done();
 };
 
 exports.MultipleImports = function(test){
-  console.log("---------- STUB");
-  test.done();
+    console.log("---------- STUB");
+    test.done();
 };
 
 exports.ImportDotNames  = function(test){
-  console.log("---------- STUB");
-  test.done();
+    console.log("---------- STUB");
+    test.done();
 };
 
 //----------
@@ -65,8 +74,8 @@ exports.ImportDotNames  = function(test){
 
 //Empty test
 exports.BehavingEntityEmptyTest = function(test){
-  console.log("---------- STUB");
-  test.done();
+    console.log("---------- STUB");
+    test.done();
 };
 
 

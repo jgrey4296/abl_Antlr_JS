4645b918ca23cec3816ce53ef64f57a3ad644073
John Grey <jgrey@soe.ucsc.edu>
2015-09-13 01:08:24 -0700

    nodeMain writes to json. PARSER V1.

--- js/nodeMain.js
+++ js/nodeMain.js
@@ -17,4 +17,14 @@ fs.readFile(process.argv[2],'utf8',function(err,data){
     var result = ABLModule.parse(data);
     console.log("Result:",result);
 
+    //save the data to json:
+    var outFileName = process.argv[2].replace(".abl",".json");
+    fs.writeFile(outFileName,JSON.stringify(result,null,4),function(err){
+        if(err){
+            return console.log(err);
+        }
+        console.log("Data written to: ",outFileName);
+    });
+
+    
 });

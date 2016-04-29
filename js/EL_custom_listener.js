2ea810df553bd989503fd00f0075667f7cf38edc
John Grey <jgrey@ucsc.edu>
2016-04-28 22:43:50 -0700

    changed over to use AMD format

--- js/EL_custom_listener.js
+++ js/EL_custom_listener.js
@@ -1,35 +1,40 @@
-
 /**
    @module ABLParser
- */
-var ELListener = require('./ELListener');
+*/
+if(typeof define !== 'function'){
+    var define = require('amdefine')(module);
+}
 
+define(['./ELListener'],function(ELListener){
+    "use strict";
+    /**
+       The JGListener constructor
+       @class JGListener
+       @constructor
+    */
+    var Listener = function(){
+        //Not ABLListener.call:
+        ELListener.ELListener.call(this);
+        this.parsedStack = [];
+        return this;
+    };
 
-/**
-   The JGListener constructor
-   @class JGListener
-   @constructor
- */
-var Listener = function(){
-    //Not ABLListener.call:
-    ELListener.ELListener.call(this);
-    this.parsedStack = [];
-    return this;
-};
+    //Not ABLListener.prototype, just ABLListener
+    //Setup the prototype chain:
+    Listener.prototype = Object.create(ELListener.ELListener.prototype);
+    Listener.prototype.constructor = Listener;
 
-//Not ABLListener.prototype, just ABLListener
-//Setup the prototype chain:
-Listener.prototype = Object.create(ELListener.ELListener.prototype);
-Listener.prototype.constructor = Listener;
+    //------------------------------
+    //PARSING METHODS:
 
-//------------------------------
-//PARSING METHODS:
+    Listener.prototype.enterEL_Program = function(ctx){
+        
+    };
 
-Listener.prototype.enterEL_Program = function(ctx){
-    console.log("ctx tostring:",ctx.getText());
-    this.parsedStack.push("entered EL Program");
-};
+    Listener.prototype.enterDotBangPair = function(ctx){
+        let dotBang = ctx.DOT() !== undefined ? 'DOT' : 'BANG';
+        this.parsedStack.push([dotBang,ctx.STRING().getText()]);
+    };
 
-
-
-exports.CustomisedListener = Listener;
+    return Listener;
+});
